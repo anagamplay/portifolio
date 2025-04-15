3bf099714db3028aae1b35589bf3a49bc3415479
AnaGamplay <anaclarasilvestre360@gmail.com>
2025-04-15 15:46:36 -0400

    Updates

---
+++ precache-manifest.cc8ca632d58747e8db9992913d9663f4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e79717a63c4869836c5aa6c77bdded",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "43d6fcd3b7bde5092fac",
+    "url": "/portifolio/static/css/main.fb22998a.chunk.css"
+  },
+  {
+    "revision": "d4a95509ff2f06a9d19a",
+    "url": "/portifolio/static/js/2.e1e23954.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/portifolio/static/js/2.e1e23954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b6c9ca6d18e9e24a7d",
+    "url": "/portifolio/static/js/3.38a4adc6.chunk.js"
+  },
+  {
+    "revision": "43d6fcd3b7bde5092fac",
+    "url": "/portifolio/static/js/main.0db39460.chunk.js"
+  },
+  {
+    "revision": "558e09cd93b75221de6e",
+    "url": "/portifolio/static/js/runtime-main.34322cec.js"
+  },
+  {
+    "revision": "9ab844347c8bc480f81c2a76766721ca",
+    "url": "/portifolio/static/media/about-me.9ab84434.png"
+  },
+  {
+    "revision": "11bf0ef4f4df1d4d3c4dc51fe50413bf",
+    "url": "/portifolio/static/media/cola-aqui-tumb.11bf0ef4.png"
+  },
+  {
+    "revision": "f20710bc696efc3099bd81cdac5973c2",
+    "url": "/portifolio/static/media/ifms-tumb.f20710bc.png"
+  },
+  {
+    "revision": "e92e9b1517ac8057ed07cf720b8ffc57",
+    "url": "/portifolio/static/media/my-expertise-bg.e92e9b15.png"
+  },
+  {
+    "revision": "7228dd4972cccd8439d3e59a92fe472f",
+    "url": "/portifolio/static/media/paceapp-tumb.7228dd49.png"
+  },
+  {
+    "revision": "9332e26ea7fe5e83a024c35d770d8125",
+    "url": "/portifolio/static/media/portifolio-tumb.9332e26e.png"
+  },
+  {
+    "revision": "f3d030966145d54ca963abacbb564334",
+    "url": "/portifolio/static/media/profile.f3d03096.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b7713c17da1e19d12e996c346dfcc4b3.js"
+  "/portifolio/precache-manifest.cc8ca632d58747e8db9992913d9663f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portifolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
